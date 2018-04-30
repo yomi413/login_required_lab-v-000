988e29f6a3664cc53080a1bccf5d51b18f219d47
Yomaira Escano <yjescano@gmail.com>
2018-04-30 03:36:04 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -10,6 +10,6 @@ class SecretsController < ApplicationController
   private
 
   def require_login
-    # return head(:forbidden) unless session.include? :user_id
+    return head(:forbidden) unless session.include? :user_id
   end
 end
