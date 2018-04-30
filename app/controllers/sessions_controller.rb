b1ca03b920f5848891fcd0093e5e7334750a474f
Yomaira Escano <yjescano@gmail.com>
2018-04-30 03:12:24 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     if params[:name].nil? || params[:name].empty?
       redirect_to '/login'
-  #   else
-  #     session[:name] = params[:name]
+    else
+      session[:name] = params[:name]
     end
   end
 end
