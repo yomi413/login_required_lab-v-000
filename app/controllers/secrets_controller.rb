26f1680b4e0789cfc952353ada9dbfff56776a01
Yomaira Escano <yjescano@gmail.com>
2018-04-30 03:45:26 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,15 +1,9 @@
 class SecretsController < ApplicationController
-  before_action :require_login
-
+  
   def show
     if !current_user
       redirect_to '/login'
     end
   end
 
-  private
-
-  def require_login
-    return head(:forbidden) unless session.include? :user_id
-  end
 end
