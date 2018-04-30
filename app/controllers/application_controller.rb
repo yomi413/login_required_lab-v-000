15626ab9d751fae57e2258865d8efb707cf3136f
Yomaira Escano <yjescano@gmail.com>
2018-04-30 03:39:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    session[:name] = params[:name]
+    session[:name]
   end
 end
